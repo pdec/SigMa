92913a93999e57231bac6128830fd7a0715e63ef
pdec <p.decewicz@uw.edu.pl>
2022-10-13 14:43:23 +1030

    Clustering nucleotide sequences. Minor corrections.

--- scripts/prepdb.py
+++ scripts/prepdb.py
@@ -6,6 +6,7 @@ import argparse
 import os
 import sys
 import shutil
+from typing import List
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from SigMaModules.read import parse_fasta, parse_genbank
@@ -39,10 +40,42 @@ def mmseqs_cluster(dbname : str, fasta_path : str, tmp_dir : str, sens : float, 
     """
 
     # call MMseqs command
-    cmd = f"mmseqs easy-cluster -v 0 -s {sens} --cov-mode {cov_mode} -c {cov} --min-seq-id {id} -e {evalue} --threads {threads} {fasta_path} {os.path.join(tmp_dir, dbname)} {os.path.join(tmp_dir, 'protcltml')}"
+    cmd = f"mmseqs easy-cluster -v 2 -s {sens} --cov-mode {cov_mode} -c {cov} --min-seq-id {id} -e {evalue} --threads {threads} {fasta_path} {os.path.join(tmp_dir, dbname)} {os.path.join(tmp_dir, 'protcltml')}"
     call_process(cmd)
 
     return os.path.join(tmp_dir, f"{dbname}_rep_seq.fasta")
+
+def meshclust_cluster(fasta_path : str, clusters_path :str, nt_id : float, threads : int) -> List[str]:
+    """
+    Runs MeShClust to cluster nuclotide sequences.
+    :param fasta_path: path to fasta file
+    :param clusters_path: path to output file
+    :param nt_id: sequence identity threshold
+    :param threads: number of threads
+    """
+
+    # call MeShClust command
+    cmd = f"meshclust -d {fasta_path} -o {clusters_path} -t {nt_id} -a n -c {threads}"
+    call_process(cmd)
+
+    clusters_ids = []
+    C_cnt = 0
+    M_cnt = 0
+    E_cnt = 0
+    with open(clusters_path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line.endswith('C'):
+                clusters_ids.append(line.split()[1][1:])
+                C_cnt += 1
+            elif line.endswith('M'):
+                M_cnt += 1
+            elif line.endswith('E'):
+                E_cnt += 1
+
+    log_progress(f"MeShClust: {C_cnt} clusters created, {M_cnt} cluster members, {E_cnt} cluster extended members", loglevel="INFO", msglevel=1)
+
+    return clusters_ids
 
 def main():
 
@@ -54,14 +87,15 @@ def main():
     parser.add_argument('--gb', help='GenBank file', nargs = '+', metavar = '<path>')
     parser.add_argument('--fasta_aa', help='Protein FASTA file', nargs = '+', metavar = '<path>')
     parser.add_argument('--fasta_nt', help='Nucleotide FASTA file', nargs = '+', metavar = '<path>')
-    parser.add_argument('--dbdir', help='Output directory', metavar = '<path>', required=True)
-    parser.add_argument('--sens', help='Sensitivity (0.0-7.5) [%(default)0.1f]', metavar = ' ', type=float, default=7.5)
-    parser.add_argument('--id', help='Identity threshold (0.0-1.0) [%(default)0.2f]', metavar = ' ', type=float, default=0.9)
+    parser.add_argument('--dbdir', help='Output directory', metavar = '<path>', required = True)
+    parser.add_argument('--sens', help='Sensitivity (0.0-7.5) [%(default)0.1f]', default = 7.5, metavar = ' ', type = float)
+    parser.add_argument('--id', help='Identity threshold (0.0-1.0) [%(default)0.2f]', default = 0.9, metavar = ' ', type = float)
+    parser.add_argument('--nt_id', help='Nucleotide identity threshold (0.0-1.0) [%(default)0.2f]', default = 0.8, metavar = ' ', type = float)
     parser.add_argument('--cov', help='Minimum coverage (0.0-1.0) [%(default)0.2f]', default = 0.8, metavar = ' ', type = float)
     parser.add_argument('--cov_mode', help='Coverage mode [%(default)i]', default = 0, metavar = ' ', type = int)
     parser.add_argument('--evalue', help='Maximum e-value [%(default)1.0e]', default = 1e-3, metavar = ' ', type = float)
     parser.add_argument('--threads', help='Number of threads [%(default)i]', default = 4, metavar = ' ', type = int)
-    parser.add_argument('--tmp', help='Temporary directory', metavar = '<path>', required=True)
+    parser.add_argument('--tmp', help='Temporary directory', metavar = '<path>', required = True)
     parser.add_argument('--log', help='Log file', metavar = '<path>')
 
     args = parser.parse_args()
@@ -87,6 +121,8 @@ def main():
         args.logger = create_logger(args.log)
     else:
         args.logger = create_logger(os.path.join(args.dbdir, "prepdb.log"))
+    
+    log_progress("Preparing reference datasets for SigMa.", loglevel="INFO", msglevel=0)
 
     # input files for clustering
     aa_fastas = []
@@ -150,11 +186,28 @@ def main():
         log_progress(f"Clustering protein sequences", loglevel = "INFO")
         for aa_fasta in aa_fastas:
             dbname = get_file_name(aa_fasta)
+            aa_cl_fasta = os.path.join(args.dbdir, f"{dbname}.fasta")
             log_progress(f"Clustering {aa_fasta}", loglevel = "INFO", msglevel=1)
             rep_seq_fasta = mmseqs_cluster(dbname, aa_fasta, args.tmp, args.sens, args.cov, args.cov_mode, args.id, args.evalue, args.threads)
-            shutil.move(rep_seq_fasta, os.path.join(args.dbdir, f"{dbname}.aa.fasta"))
+            records = parse_fasta(rep_seq_fasta)
+            log_progress(f"MMSeqs: {len(records)} clusters created", loglevel = "INFO", msglevel=1)
+            write_fasta(records, aa_cl_fasta)
 
+    # cluster nucleotide sequences
+    if nt_fastas:
+        log_progress(f"Clustering nucleotide sequences", loglevel = "INFO")
+        for nt_fasta in nt_fastas:
+            dbname = get_file_name(nt_fasta)
+            nt_cl_fasta = os.path.join(args.dbdir, f"{dbname}.fasta")
+            log_progress(f"Clustering {nt_fasta}", loglevel = "INFO", msglevel=1)
+            clusters_file = os.path.join(args.tmp, f"{dbname}.meshclusters.tsv")
+            clusters_ids = meshclust_cluster(nt_fasta, clusters_file, args.nt_id, args.threads)
+            write_fasta([record for record in parse_fasta(nt_fasta) if record.id in clusters_ids], nt_cl_fasta)
 
+    log_progress(f"Removing temporary directory {args.tmp}", loglevel = "INFO")
+    shutil.rmtree(args.tmp)
+
+    log_progress("Done! You can now run use output files for SigMa!", loglevel = "INFO", msglevel=0)
 
 if __name__ == '__main__':
     main()
